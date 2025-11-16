8c8b564f43e0124ac4ac71a8d31648ccd2e96f7d
UdayParkar <udayparkar2003@gmail.com>
2025-11-16 12:22:43 +0530

    feat(terraform): open ports for monitoring (3000, 9090, 9100)

--- terraform/main.tf
+++ terraform/main.tf
@@ -68,6 +68,30 @@ resource "aws_security_group" "app_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+    ingress {
+    description = "Prometheus UI"
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Grafana UI"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Node Exporter Metrics"
+    from_port   = 9100
+    to_port     = 9100
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     description = "All egress"
     from_port   = 0
