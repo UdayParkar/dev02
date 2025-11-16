b6f43ee8a1697955279d6149e9f0135077ae452b
UdayParkar <udayparkar2003@gmail.com>
2025-11-16 15:27:13 +0530

    added sg of cadviser

--- terraform/main.tf
+++ terraform/main.tf
@@ -92,6 +92,14 @@ resource "aws_security_group" "app_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "cAdvisor UI"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     description = "All egress"
     from_port   = 0
